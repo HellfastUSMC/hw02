aacd01b68be4d0d079fa94c6d7003ccee9f52a70
Alexander Nabiev <4epenki@gmail.com>
2021-10-25 14:25:36 +0300

    mReview v1

--- yatube/groups/models.py
+++ yatube/groups/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.db.models.fields import CharField, SlugField, TextField
 from django.contrib.auth import get_user_model
-from django.db.models.fields.related import ForeignKey
 
 User = get_user_model()
 
@@ -10,5 +9,6 @@ class Group(models.Model):
     title = CharField(max_length=200)
     slug = SlugField(unique=True)
     description = TextField()
+    
     def __str__(self):
         return self.title
--- yatube/groups/urls.py
+++ yatube/groups/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 
 app_name = 'groups'
+
 urlpatterns = [
     path('<slug:slug>/', views.group_posts, name='group_list'),
 ]
