f3e5ca920ff63d02c56ebd08f082367a98a5da10
Alexander Nabiev <4epenki@gmail.com>
2021-11-02 17:49:01 +0300

    lessons2

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -5,6 +5,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
     path('group/', include('groups.urls', namespace='groups')),
     path('', include('posts.urls', namespace='posts')),
 ]
